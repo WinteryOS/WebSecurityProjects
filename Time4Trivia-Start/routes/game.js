c89932361cf7d183998146ebc67986088e91b278
Kolby <winteryrs@gmail.com>
2022-08-03 18:01:55 -0600

    Added basic Trivia Question display

--- Time4Trivia-Start/routes/game.js
+++ Time4Trivia-Start/routes/game.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoDAL = require('../data/mongoDAL')
 const router = express.Router();
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://Kolby123:Kolby123@cluster0.zsj8j.mongodb.net/test?retryWrites=true&w=majority';
 
-router.get('/play', function(req, res, next) {
-  // TODO: Implement Game
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+router.get('/play', async function(req, res, next) {
+  var questionsArray = await mongoDAL.getAllQuestions()
+  console.log("This is the questions array: " +  questionsArray)
   
-  res.render('play', {user: req.session.user});
+  res.render('play', { title: 'Time 4 Trivia',user: req.session.user, questions: questionsArray});
 });
+
 
 module.exports = router;
--- Time4Trivia-Start/routes/index.js
+++ Time4Trivia-Start/routes/index.js
@@ -5,6 +5,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Time 4 Trivia', user: req.session.user });
 });
 
+
+
 router.get('/leaderboard', function(req, res, next) {
   // TODO: Get actual leader data from the MONGO database!
   let leaders = [
